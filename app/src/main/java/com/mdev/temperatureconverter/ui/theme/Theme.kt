c5e04087a1676b0c84b120db5a737c71e14d1d1b
Wallace Balaniuc <wallace.balaniuc@gmail.com>
2022-11-23 09:47:59 -0500

    Initial commit

---
+++ app/src/main/java/com/mdev/temperatureconverter/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.mdev.temperatureconverter.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = charcol,
+    primaryVariant = charcol,
+    secondary = charcol
+)
+
+private val LightColorPalette = lightColors(
+    primary = charcol,
+    primaryVariant = charcol,
+    secondary = charcol
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun TemperatureConverterTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
